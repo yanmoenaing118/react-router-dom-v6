7376a4a3768ed2879223109c16330c7b70abc82e
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-03-07 16:50:32 +0630

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,16 @@
-import { motion, useAnimation } from "framer-motion";
+import "./App.css";
+import { motion, useAnimation, AnimatePresence } from "framer-motion";
 import { FaAngleDoubleUp } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 export default function App() {
   const boxControls = useAnimation();
 
-  const list = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: {
-        duration: 0.3,
-        when: "beforeChildren",
-        staggerChildren: 0.3,
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: {
-      x: -500,
-      opacity: 0,
-      scale: 1
-    },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        duration: 0.3,
-        type: "tween",
-      },
-    },
-    hovered: {
-      scale: 1.05
-    },
-
-    active: {
-      scale: 1
-    }
-  };
+  const [show, setShow] = useState(false);
 
   return (
-    <main>
-      <motion.ul
-        style={{
-          listStyle: "none",
-          padding: "0",
-          fontSize: "2rem",
-        }}
-        initial="hidden"
-        animate="visible"
-        variants={list}
-      >
-        {[1, 2, 3, 4, 5].map((item) => (
-          <motion.li
-            key={item}
-            style={{
-              margin: "1rem 0",
-              backgroundColor: "#fff",
-              color: "#333",
-              padding: ".3em .6em",
-              borderRadius: "5px",
-              cursor: "pointer"
-            }}
-            variants={itemVariants}
-            whileHover="hovered"
-          >
-            Item {item}
-          </motion.li>
-        ))}
-      </motion.ul>
-
+    <main onClick={() => setShow(!show)}>
+      <Component isVisible={show} />
       <style jsx>{`
         main {
           width: 100%;
@@ -84,5 +23,32 @@ export default function App() {
         }
       `}</style>
     </main>
+  );
+}
+
+function Component({ isVisible }) {
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+        key="item"
+          initial={{
+            opacity: 0,
+            y: "-100vh"
+          }}
+          animate={{
+            opacity: 1,
+            y: 0
+          }}
+          exit={{
+            opacity: 0,
+            y: "-100vh"
+          }}
+          className="item"
+        >
+          Box
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
